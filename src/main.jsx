77f21687f5be01239ada66b75b08f45b3470f9d3
behlil <bahlil2001@gmail.com>
2023-03-08 19:15:18 +0100

    Fix refresh error with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, Link, BrowserRouter, HashRouter } from 'react-router-dom'
 
 import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
