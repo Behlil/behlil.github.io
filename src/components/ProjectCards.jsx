22aa45722f01b1bb21151f8de974038cbca3e1e1
behlil <bahlil2001@gmail.com>
2023-03-08 12:47:33 +0100

    modify: preojectsCards file

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,11 +1,63 @@
 import React, { useState } from 'react';
 import Card from './Card';
-import projectsData from '../data/Data';
+// import projectsData from '../data/Data';
 import './ProjectCards.css'
 import FilterBar from './FilterBar';
+import img1 from '../Images/analysis.png';
+import img2 from '../Images/powerbi.png';
 
 
 export default function ProjectCards() {
+    const projectsData = [
+        {
+            id: 'HousePriceCompetition',
+            title: 'House Price Competition',
+            description: 'This is the description for project 1.',
+            image: img1,
+            url: 'https://github.com/Behlil/House-Price-Competition/blob/main/notebook.ipynb',
+            technologies: ['Python'],
+        },
+        {
+            id: 2,
+            title: 'Project 2',
+            description: 'This is the description for project 2.',
+            image: img2,
+            url: 'https://example.com/project2',
+            technologies: ['Python', 'SQL'],
+        },
+        {
+            id: 3,
+            title: 'Project 3',
+            description: 'This is the description for project 3.',
+            image: 'https://via.placeholder.com/150',
+            url: 'https://example.com/project3',
+            technologies: ['Python', 'SQL', 'Power BI'],
+        },
+        {
+            id: 4,
+            title: 'Project 4',
+            description: 'This is the description for project 4.',
+            image: 'https://via.placeholder.com/150',
+            url: 'https://example.com/project3',
+            technologies: ['Python', 'SQL', 'Power BI'],
+        },
+        {
+            id: 5,
+            title: 'Project 5',
+            description: 'This is the description for project 5.',
+            image: 'https://via.placeholder.com/150',
+            url: 'https://example.com/project3',
+            technologies: ['Python', 'SQL', 'Power BI'],
+        },
+        {
+            id: 6,
+            title: 'Project 6',
+            description: 'This is the description for project 6.',
+            image: 'https://via.placeholder.com/150',
+            url: 'https://example.com/project3',
+            technologies: ['Python', 'SQL', 'Power BI'],
+        },
+    ];
     const [selectedTechnology, setSelectedTechnology] = useState('all');
     const [filteredProjects, setFilteredProjects] = useState(projectsData); // <--- add this line
 
@@ -47,8 +99,8 @@ export default function ProjectCards() {
                     <Card key={project.id} project={project} />
                 ))}
             </div>
-            
-            
+
+
         </div>
     );
 }
