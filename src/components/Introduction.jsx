e614f4c19125a06a6320909b99f758e20bdf19be
behlil <bahlil2001@gmail.com>
2023-09-15 21:09:04 +0100

    modify the font size and intro

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,63 +1,84 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Create a styled component for the introduction section
-const Introduction = styled.section`
+// Define your styled components
+const IntroductionContainer = styled.div`
+  width: 100%;
+  padding: 0 2em;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  justify-content: center;
-  background-color: #F6F1F1;
-  padding: 2rem;
-  height: 90vh;
-  margin: 0 ;
-
-  @media (max-width: 768px) {
-    height: auto;
-  }
+  background-color: #fefefe;
 `;
 
-// Create a styled component for the heading
-const Heading = styled.h1`
+const LeftSection = styled.div`
+  width: 60%;
+  padding: 20px;
+`;
+
+const RightSection = styled.div`
+  width: 50%;
+`;
+
+const Title = styled.h1`
   font-size: 4rem;
-  color: #454545;
-  margin-top: 4rem;
+  margin-bottom: 20px;
 
-  @media (max-width: 768px) {
-    // margin-top: 4rem;
-    font-size: 2rem;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    // color: #ff5722; /* Change to your desired hover color */
+  }
+`;
+const SubTitle = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 20px;
+
+`;
+
+const Description = styled.p`
+  transition: color 0.3s ease-in-out;
+
+  
+`;
+
+const DownloadButton = styled.a`
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #0e0e0e; /* Change to your desired button color */
+  color: #fff;
+  text-decoration: none;
+  border-radius: 4px;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #d84315; /* Change to your desired hover color */
   }
 `;
 
-// Create a styled component for the paragraph
-const Paragraph = styled.p`
-  font-size: 1.5rem;
-  color: #666666;
-  text-align: left;
-  max-width: 800px;
-
-  @media (max-width: 768px) {
-    font-size: 1rem;
-  }
+const Photo = styled.img`
+  width: 100%;
+  max-width: 300px;
+  animation: rotate 6s linear infinite;
 `;
 
-
-const Intro = () => {
+const Introduction = () => {
   return (
-    <Introduction>
-      <Heading>About me</Heading>
-      <Paragraph>
-        As a curious mind with a passion for problem-solving, I stumbled upon the world
-        of data analytics while delving into the intricacies of mathematics and probability.
-        What started as a mere curiosity quickly turned into an all-consuming passion for telling
-        compelling stories through data visualization and making complex concepts appear effortless.
-        My journey as a data analyst has taught me that the true art of data lies not just in numbers,
-        but in the ability to communicate insights and drive results. And so, armed with my love for
-        numbers, my eye for design, and my passion for creating impact,I am eager to take on the
-        world of data analytics and make my mark in this exciting field.
-      </Paragraph>
-    </Introduction>
+    <IntroductionContainer>
+      <LeftSection>
+        <Title>Behlil Ouassim</Title>
+        <SubTitle>Data Analyst</SubTitle>
+        <Description>As a curious mind with a passion for problem-solving, I stumbled upon the world of data analytics while delving into the intricacies of mathematics and probability. What started as a mere curiosity quickly turned into an all-consuming passion for telling compelling stories through data visualization and making complex concepts appear effortless. My journey as a data analyst has taught me that the true art of data lies not just in numbers, but in the ability to communicate insights and drive results. And so, armed with my love for numbers, my eye for design, and my passion for creating impact,I am eager to take on the world of data analytics and make my mark in this exciting field.</Description>
+        <DownloadButton href="/path-to-your-CV.pdf" target="_blank" rel="noopener noreferrer">
+          Download CV
+        </DownloadButton>
+      </LeftSection>
+      <RightSection>
+        <Photo src="https://th.bing.com/th/id/OIP.ZpVunDDzxPZxFy630A-3RwHaLH?w=207&h=305&c=7&r=0&o=5&pid=1.7" alt="Your Photo" />
+      </RightSection>
+    </IntroductionContainer>
   );
 };
 
-export default Intro;
+export default Introduction;
